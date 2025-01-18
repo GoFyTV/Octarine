620657db60a86049f05ec58d153fb1af6ea01dfd
GoFyTV <valera.gofy@gmail.com>
2025-01-18 23:30:20 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import {
 	EventsSDK,
-	NotificationsSDK
+	Menu
 } from "github.com/octarine-public/wrapper/index"
 
 // NotificationsSDK.Push()
@@ -8,4 +8,29 @@ import {
 console.log("Hello world!")
 EventsSDK.on("GameStarted", () => {
 	console.log("Hello world!")
+})
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	var INTERNAL_DEBUGGER_Skip: (seconds: number) => void
+}
+
+export const RootMenu = Menu.AddEntry(
+	"Debugger GUI",
+	"panorama/images/plus/achievements/battlecup_icon_png.vtex_c"
+)
+RootMenu.AddKeybind("Emit fake Tick").OnRelease(() => {
+	EventsSDK.emit("PreDataUpdate", false)
+	EventsSDK.emit("MidDataUpdate", false)
+	EventsSDK.emit("PostDataUpdate", false)
+	EventsSDK.emit("Tick", false, 0)
+})
+
+const SkipMenu = RootMenu.AddNode("Skip into the game")
+const SkipMinute = SkipMenu.AddSlider("Minute", 5, 0, 120),
+	SkipSecond = SkipMenu.AddSlider("Second", 0, 0, 59)
+SkipMenu.AddButton("Skip").OnValue(() => {
+	if (globalThis.INTERNAL_DEBUGGER_Skip !== undefined) {
+		INTERNAL_DEBUGGER_Skip(SkipMinute.value * 60 + SkipSecond.value)
+	}
 })
